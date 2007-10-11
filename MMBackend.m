f48d84017f685e88b951ceb564244ed645b8ab57
Bjorn Winckler <bjorn.winckler@gmail.com>
2007-10-11 18:37:42 +0000

    Now works to bind to <C-Enter>, <S-Enter>, <C-S-Enter>
    git-svn-id: http://macvim.googlecode.com/svn/trunk@307 96c4425d-ca35-0410-94e5-3396d5c13a8f

--- MMBackend.m
+++ MMBackend.m
@@ -1885,7 +1885,7 @@ enum {
         // Unfortunately, the only way to deal with when to clear the modifiers
         // or not seems to be to have hard-wired rules like this.
         if ( !((' ' == c) || (0xa0 == c) || (mods & MOD_MASK_CMD)
-                    || 0x9 == c) ) {
+                    || 0x9 == c || 0xd == c) ) {
             mods &= ~MOD_MASK_SHIFT;
             mods &= ~MOD_MASK_CTRL;
             //NSLog(@"clear shift ctrl");
