5aaf05255c5dc98a04ce49489d8e8f35fee76b1c
Bjorn Winckler <bjorn.winckler@gmail.com>
2007-11-07 19:46:46 +0100

    Always open a dropped file in new tab
    Always use ":tab drop" when dropping files.  (Used to call ":drop" when only
    one file was dropped but this would not open the file in a new tab.)

--- src/MacVim/MMBackend.m
+++ src/MacVim/MMBackend.m
@@ -2010,9 +2010,7 @@ enum {
         // HACK!  I'm not sure how to get Vim to open a list of files in
         // tabs, so instead I create a ':tab drop' command with all the
         // files to open and execute it.
-        NSMutableString *cmd = (n > 1)
-                ? [NSMutableString stringWithString:@":tab drop"]
-                : [NSMutableString stringWithString:@":drop"];
+        NSMutableString *cmd = [NSMutableString stringWithString:@":tab drop"];
 
         int i;
         for (i = 0; i < n && bytes < end; ++i) {
