ee5a7d0bb73c0274431ca3601e5b6af072c59239
Bjorn Winckler <bjorn.winckler@gmail.com>
2008-02-13 18:03:58 +0100

    Add script that builds a new snapshot
    The script make-snapshot.sh will build a new snapshot, then create a
    folder on the Desktop containing
        MacVim.app
        README-snapshot.txt
        mvim
    The folder is then archived and compressed (to a file on the Desktop).

---
+++ src/MacVim/README-snapshot.txt
@@ -0,0 +1,7 @@
+This archive contains a snapshot of 'MacVim.app' and the shell script
+'mvim'.
+
+For usage instructions, please start MacVim and type ":h macvim"
+(without the quotes).
+
+MacVim requires a PPC or Intel Mac running Mac OS X 10.4 or 10.5.
---
+++ src/MacVim/make-snapshot.sh
@@ -0,0 +1,53 @@
+#!/bin/sh
+
+# Increment build number
+/Developer/Tools/agvtool next-version -all
+
+# Get current build number
+BUILDNUM=`/Developer/Tools/agvtool what-version -terse`
+DEST=~/Desktop/MacVim-snapshot-$BUILDNUM
+
+# Build Vim binary
+echo 'BUILDING VIM BINARY'
+echo '    running configure...'
+cd .. && ./configure --enable-gui=macvim --with-mac-arch=both \
+    --with-features=huge --enable-pythoninterp --enable-tclinterp \
+    --enable-cscope \
+    --with-compiledby="Bjorn Winckler <bjorn.winckler@gmail.com>" > /dev/null
+
+echo '    cleaning...'
+make clean > /dev/null
+echo '    calling make...'
+make > /dev/null
+echo '    done'
+
+# Build MacVim.app
+echo 'BUILDING MacVim.app'
+cd MacVim
+echo '    cleaning...'
+xcodebuild -configuration Universal clean > /dev/null
+echo '    calling xcodebuild...'
+xcodebuild -configuration Universal > /dev/null
+echo '    done'
+
+# Create archive of build/Universal/MacVim.app
+echo 'CREATING SNAPSHOT ARCHIVE'
+echo '    copying MacVim.app and supporting files...'
+mkdir $DEST
+cp -pR build/Universal/MacVim.app $DEST/
+cp -p mvim $DEST/
+cp -p README-snapshot.txt $DEST/
+echo '    creating archive....'
+cd $DEST && cd ..
+tar cjf MacVim-snapshot-$BUILDNUM.tbz MacVim-snapshot-$BUILDNUM
+echo '    done'
+
+echo 'ALL DONE'
+echo 'Now update the Appcast, commit and tag, then post on vim_mac.'
+# Update app-cast
+
+# Commit & tag
+# git-commit -a -m "$BUILDNUM"
+# git-tag -a -F tagfile $BUILDNUM
+
+# Post on vim_mac
