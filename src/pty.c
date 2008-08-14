a689202f4eecae37798c929289bbbca7ffea2a58
Ben Schmidt <mail_ben_schmidt@yahoo.com.au>
2008-08-14 12:45:41 +0200

    Fix Leopard pty problems
    Problem: Pseudo TTYs (ptys) are not successfully allocated on Leopard
             (or when Vim is compiled on Leopard).
    Solution: Do not attempt to use SVR4-compatible ptys on Mac OS X even
              though configure may detect their presence.

--- src/pty.c
+++ src/pty.c
@@ -270,9 +270,11 @@ OpenPTY(ttyn)
 }
 #endif
 
-#if defined(HAVE_SVR4_PTYS) && !defined(PTY_DONE) && !defined(hpux)
+#if defined(HAVE_SVR4_PTYS) && !defined(PTY_DONE) && !defined(hpux) && !defined(MACOS_X)
 
 /* NOTE: Even though HPUX can have /dev/ptmx, the code below doesn't work! */
+/* NOTE: Also with Mac OS X, I believe a compatibility mode can be chosen
+         that uses /dev/ptmx but this is not done, so we can't use this. */
 #define PTY_DONE
     int
 OpenPTY(ttyn)
