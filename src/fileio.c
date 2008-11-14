6319b9c5177f0e632879433f11ba1b4fad0de75d
Bjorn Winckler <bjorn.winckler@gmail.com>
2008-11-14 20:06:53 +0100

    Add Reload/Ignore All buttons to file changed dialog

--- src/fileio.c
+++ src/fileio.c
@@ -6232,6 +6232,9 @@ vim_rename(from, to)
 }
 
 static int already_warned = FALSE;
+#ifdef FEAT_GUI_MACVIM
+static int default_reload_choice = 0;
+#endif
 
 /*
  * Check if any not hidden buffer has been changed.
@@ -6274,6 +6277,9 @@ check_timestamps(focus)
 	++no_wait_return;
 	did_check_timestamps = TRUE;
 	already_warned = FALSE;
+#ifdef FEAT_GUI_MACVIM
+	default_reload_choice = 0;
+#endif
 	for (buf = firstbuf; buf != NULL; )
 	{
 	    /* Only check buffers in a window. */
@@ -6292,6 +6298,9 @@ check_timestamps(focus)
 	    }
 	    buf = buf->b_next;
 	}
+#ifdef FEAT_GUI_MACVIM
+	default_reload_choice = 0;
+#endif
 	--no_wait_return;
 	need_check_timestamps = FALSE;
 	if (need_wait_return && didit == 2)
@@ -6551,9 +6560,33 @@ buf_check_timestamp(buf, focus)
 		    STRCAT(tbuf, "\n");
 		    STRCAT(tbuf, mesg2);
 		}
+# ifdef FEAT_GUI_MACVIM
+		if (default_reload_choice > 0)
+		{
+		    if (default_reload_choice == 2)
+			reload = TRUE;
+		}
+		else
+		{
+		    switch (do_dialog(VIM_WARNING, (char_u *)_("Warning"), tbuf,
+					(char_u *)_("&OK\n&Load File\nLoad &All\n&Ignore All"),
+								    1, NULL))
+		    {
+			case 3:
+			    default_reload_choice = 2;
+			case 2:
+			    reload = TRUE;
+			    break;
+			case 4:
+			    default_reload_choice = 1;
+			    break;
+		    }
+		}
+# else
 		if (do_dialog(VIM_WARNING, (char_u *)_("Warning"), tbuf,
 				(char_u *)_("&OK\n&Load File"), 1, NULL) == 2)
 		    reload = TRUE;
+# endif
 	    }
 	    else
 #endif
