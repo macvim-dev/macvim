kana/vim

commit 2d497eabe995ed7f667d67166b396dff6389d12f

Fix "set go+=rT" bug
The number of columns now matches the width of the window after calling
"set go+=rT".

src/MacVim/MMWindowController.h
@@ -22,6 +22,7 @@
     MMVimView           *vimView;
     BOOL                setupDone;
     BOOL                shouldResizeVimView;
+    int                 shouldUpdateToolbar;
     BOOL                keepOnScreen;
     BOOL                fullscreenEnabled;
     NSString            *windowAutosaveKey;

src/MacVim/MMWindowController.m
@@ -81,6 +81,7 @@
 - (BOOL)askBackendForStarRegister:(NSPasteboard *)pb;
 - (void)hideTablineSeparator:(BOOL)hide;
 - (void)doFindNext:(BOOL)next;
+- (void)updateToolbar;
 @end
 
 
@@ -428,6 +429,9 @@
     // If we were to immediately resize then the vim view size would jitter
     // (e.g.  hiding/showing scrollbars often happens several time in one
     // update).
+    // Also delay toggling the toolbar until after scrollbars otherwise
+    // problems arise when showing toolbar and scrollbar at the same time, i.e.
+    // on "set go+=rT".
 
     if (shouldResizeVimView) {
         shouldResizeVimView = NO;
@@ -447,6 +451,11 @@
         }
 
         keepOnScreen = NO;
+    }
+
+    if (shouldUpdateToolbar != 0) {
+        [self updateToolbar];
+        shouldUpdateToolbar = 0;
     }
 }
 
@@ -481,20 +490,16 @@
 
     [toolbar setSizeMode:size];
     [toolbar setDisplayMode:mode];
-    [toolbar setVisible:on];
 
-    if (([decoratedWindow styleMask] & NSTexturedBackgroundWindowMask) == 0) {
-        if (!on) {
-            [self hideTablineSeparator:YES];
-        } else {
-            [self hideTablineSeparator:![[vimView tabBarControl] isHidden]];
-        }
+    // NOTE: If the window is not visible we must toggle the toolbar
+    // immediately, otherwise "set go-=T" in .gvimrc will lead to the toolbar
+    // showing its hide animation every time a new window is opened.  (See
+    // processCommandQueueDidFinish for the reason why we need to delay
+    // toggling the toolbar when the window is visible.)
+    if ([decoratedWindow isVisible]) {
+        shouldUpdateToolbar = on ? 1 : -1;
     } else {
-        // Textured windows don't have a line below there title bar, so we
-        // need the separator in this case as well. In fact, the only case
-        // where we don't need the separator is when the tab bar control
-        // is visible (because it brings its own separator).
-        [self hideTablineSeparator:![[vimView tabBarControl] isHidden]];
+        [self updateToolbar];
     }
 }
 
@@ -1023,6 +1028,29 @@
     }
 
     [vimController addVimInput:input];
+}
+
+- (void)updateToolbar
+{
+    NSToolbar *toolbar = [decoratedWindow toolbar];
+    if (!toolbar) return;
+
+    BOOL on = shouldUpdateToolbar > 0 ? YES : NO;
+    [toolbar setVisible:on];
+
+    if (([decoratedWindow styleMask] & NSTexturedBackgroundWindowMask) == 0) {
+        if (!on) {
+            [self hideTablineSeparator:YES];
+        } else {
+            [self hideTablineSeparator:![[vimView tabBarControl] isHidden]];
+        }
+    } else {
+        // Textured windows don't have a line below there title bar, so we
+        // need the separator in this case as well. In fact, the only case
+        // where we don't need the separator is when the tab bar control
+        // is visible (because it brings its own separator).
+        [self hideTablineSeparator:![[vimView tabBarControl] isHidden]];
+    }
 }
 
 @end // MMWindowController (Private)

macvim-dev/macvim

commit 5663536b1fa2e0d5ad8c577472b310e3003da9a9

Do not hide toolbar unless requested
Commit 2d497eabe995ed7f667d67166b396dff6389d12f introduced a regression
where the toolbar automatically hid upon startup regardless whether 'go'
included the "T" flag or not.

src/MacVim/MMWindowController.h
@@ -22,7 +22,7 @@
     MMVimView           *vimView;
     BOOL                setupDone;
     BOOL                shouldResizeVimView;
-    int                 shouldUpdateToolbar;
+    int                 updateToolbarFlag;
     BOOL                keepOnScreen;
     BOOL                fullscreenEnabled;
     NSString            *windowAutosaveKey;

src/MacVim/MMWindowController.m
@@ -453,10 +453,8 @@
         keepOnScreen = NO;
     }
 
-    if (shouldUpdateToolbar != 0) {
+    if (updateToolbarFlag != 0)
         [self updateToolbar];
-        shouldUpdateToolbar = 0;
-    }
 }
 
 - (void)showTabBar:(BOOL)on
@@ -491,16 +489,16 @@
     [toolbar setSizeMode:size];
     [toolbar setDisplayMode:mode];
 
+    // Positive flag shows toolbar, negative hides it.
+    updateToolbarFlag = on ? 1 : -1;
+
     // NOTE: If the window is not visible we must toggle the toolbar
     // immediately, otherwise "set go-=T" in .gvimrc will lead to the toolbar
     // showing its hide animation every time a new window is opened.  (See
     // processCommandQueueDidFinish for the reason why we need to delay
     // toggling the toolbar when the window is visible.)
-    if ([decoratedWindow isVisible]) {
-        shouldUpdateToolbar = on ? 1 : -1;
-    } else {
+    if (![decoratedWindow isVisible])
         [self updateToolbar];
-    }
 }
 
 - (void)setMouseShape:(int)shape
@@ -1061,9 +1059,10 @@
 - (void)updateToolbar
 {
     NSToolbar *toolbar = [decoratedWindow toolbar];
-    if (!toolbar) return;
+    if (nil == toolbar || 0 == updateToolbarFlag) return;
 
-    BOOL on = shouldUpdateToolbar > 0 ? YES : NO;
+    // Positive flag shows toolbar, negative hides it.
+    BOOL on = updateToolbarFlag > 0 ? YES : NO;
     [toolbar setVisible:on];
 
     if (([decoratedWindow styleMask] & NSTexturedBackgroundWindowMask) == 0) {
@@ -1079,6 +1078,8 @@
         // is visible (because it brings its own separator).
         [self hideTablineSeparator:![[vimView tabBarControl] isHidden]];
     }
+
+    updateToolbarFlag = 0;
 }
 
 @end // MMWindowController (Private)