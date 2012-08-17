25150b07cf76675d13cd73070835f58da40e0d57
Bjorn Winckler <bjorn.winckler@gmail.com>
2012-08-17 18:57:02 +0200

    Fix static analyzer warning in DBPrefs

--- src/MacVim/DBPrefsWindowController.m
+++ src/MacVim/DBPrefsWindowController.m
@@ -392,7 +392,7 @@ static DBPrefsWindowController *_sharedPrefsWindowController = nil;
 	NSEnumerator *subviewsEnum = [[contentSubview subviews] reverseObjectEnumerator];
 	
 		// This is our visible view. Just get past it.
-	subview = [subviewsEnum nextObject];
+	(void)[subviewsEnum nextObject];
 
 		// Remove everything else. There should be just one, but
 		// if the user does a lot of fast clicking, we might have
